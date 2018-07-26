f1dc8da9a1da68414ac20390b864dc1fb456a48a
olebas1927 <olebas13@ukr.net>
2018-07-26 17:52:25 +0300

    use properties(getters and setters)

--- src/main/java/com/olebas/spring/impls/robot/ModelT1000.java
+++ src/main/java/com/olebas/spring/impls/robot/ModelT1000.java
@@ -21,6 +21,30 @@ public class ModelT1000 implements Robot {
 		this.head = head;
 	}
 
+	public Hand getHand() {
+		return hand;
+	}
+
+	public void setHand(Hand hand) {
+		this.hand = hand;
+	}
+
+	public Leg getLeg() {
+		return leg;
+	}
+
+	public void setLeg(Leg leg) {
+		this.leg = leg;
+	}
+
+	public Head getHead() {
+		return head;
+	}
+
+	public void setHead(Head head) {
+		this.head = head;
+	}
+
 	@Override
 	public void action() {
 		head.calc();
